7846ad65a0a99ca126487a186c2edb0f174935c0
Chapin <23ChapinK@mydacc.org>
2022-10-24 14:15:38 -0400

    fr

--- javascript/loan.js
+++ javascript/loan.js
@@ -1,1 +1,11 @@
-const loanAmountInput = document.querySelector("")
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureRate = document.querySelector(".loan-tenure");
+
+const loanEMIvalue = document.querySelector(".loan-emi .value");
+const totalInterestValue = document.querySelector(".loan-interest .value");
+const totalAmountValue = document.querySelector(".loan-amount .value");
+
+const calculateBtn = document.querySelector(".calculate-button");
+
+
